5cea1b6e255c2d7509b47b53daeb4fcda5fcefda
biplavt <biplavt@hawsco.com>
2019-01-14 08:18:37 -0800

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _=require('lodash');
 var express=require('express');
 var bodyParser=require('body-parser');  //changes json to object
 
@@ -72,6 +73,32 @@ app.delete('/todos/:id',(req,res)=>{
 	})
 });
 
+app.patch('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	var body= _.pick(req.body,['text','completed']); //grabs text, completed and assigns it to body
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	};
+	
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt=new Date().getTime(); //create a new property, and assign timestamp
+	}else{
+		body.completed=false;
+		body.completedAt=null;
+	}
+
+	Todo.findByIdAndUpdate(id,
+		{$set:body},{new:true}).then((todo)=>{
+			if(!todo){
+				return res.status(404).send();
+			}
+			res.send({todo});
+		}).catch((e)=>{
+			res.status(400).send();
+		})
+
+
+})
 
 
 
