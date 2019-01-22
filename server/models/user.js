c902eed0c5ce844ea65919696202412d98348b98
biplavt <biplavt@hawsco.com>
2019-01-22 10:16:18 -0800

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,67 @@
 var mongoose=require('mongoose');
+var validator=require('validator');
+var jwt=require('jsonwebtoken');
+const _= require('lodash');
 
 
-const User=mongoose.model('User',{
+var UserSchema= new mongoose.Schema({
+
 	email:{
 		type:String,
 		required:true,
 		trim:true,
-		minlength:1
-	}
-})
+		minlength:1,
+		unique:true,
+		validate:{
+			validator:(value)=>{
+				return validator.isEmail(value);
+			},
+			message:'{VALUE} isn ot a valid email'
+		}
+	},
+	password:{
+		type:String,
+		required:true,
+		minlength:6
+	},
+	tokens:[{
+		access:{
+			type:String,
+			required:true
+		},
+		token:{
+			type:String,
+			required:true
+		}
+	}]
+});
+
+UserSchema.methods.toJSON=function(){
+	var user=this;
+	var userObject =user.toObject();
+
+	return _.pick(userObject,['_id','email']);
+}
+
+UserSchema.methods.generateAuthToken=function(){
+	// console.log('here');
+	var user=this;
+	var access='auth';
+	var token=jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+	console.log('token:',token);
+
+	// user.tokens=user.tokens.concat([{access,token}]);
+	user.tokens.push({access,token});
+	// console.log('users:',user);
+
+	return user.save().then(()=>{
+		return token;
+	})
+};
+
+const User=mongoose.model('User',UserSchema);
+
+
 
 module.exports={
 	User
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ var {ObjectID}=require('mongodb');
 
 var app=express();
 
-var PORT=process.env.PORT ;
+var PORT=process.env.PORT ||3000;
 
 app.use(bodyParser.json());
 
@@ -102,6 +102,19 @@ app.patch('/todos/:id',(req,res)=>{
 
 })
 
+//POST /users
+app.post('/users',(req,res)=>{
+	var body= _.pick(req.body,['email','password']); 
+	var user=new User(body);
+
+	user.save().then(()=>{
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((err)=>{
+		res.status(400).send(err);
+	})
+})
 
 
 app.listen(PORT,()=>{
